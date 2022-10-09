bb0b41988af9c0bd79449e191b1b94cf6e0130b2
jaysujae <jaysujae@gmail.com>
2022-10-09 19:35:56 +0800

    Add README, fix code

---
+++ unbelievable-power-of-small-information/README.md
@@ -0,0 +1,3 @@
+The code for article, Unbelievable Power of Small Information 
+
+You can run the code and see that it is actually 75% by guessing the first person's answer.
--- unbelievable-power-of-small-information/main.go
+++ unbelievable-power-of-small-information/main.go
@@ -5,44 +5,61 @@ import (
 	"math/rand"
 	"time"
 )
-    var seed = rand.NewSource(time.Now().UnixNano())
-    var random = rand.New(seed)
+
+var seed = rand.NewSource(time.Now().UnixNano())
+var random = rand.New(seed)
 
 type experiment struct {
-    people []bool
-    more bool
+	people []bool
+	more   bool
 }
 
-func(e *experiment) firstPersonTaste() bool{
-    return e.people[0]
+func (e *experiment) firstPersonTaste() bool {
+	return e.people[0]
 }
 
-func NewExperiment(peopleNum int) experiment{
-    people := make([]bool, peopleNum)
-    numberOfPeopleWhoLike := rand.Intn(peopleNum+1)
-    randomPermutationSlice := rand.Perm(peopleNum)
-    for _, num := range randomPermutationSlice[:numberOfPeopleWhoLike] {
-        people[num] = true
-    }
-    more := false
-    if numberOfPeopleWhoLike > (peopleNum / 2){
-        more = true
-    }
-    resp := experiment{
-        people: people,
-        more : more,
-    }
-    return resp
+func NewExperiment(peopleNum int) experiment {
+	people := make([]bool, peopleNum)
+	numberOfPeopleWhoLike := rand.Intn(peopleNum + 1)
+	randomPermutationSlice := rand.Perm(peopleNum)
+	for _, num := range randomPermutationSlice[:numberOfPeopleWhoLike] {
+		people[num] = true
+	}
+	more := false
+	if numberOfPeopleWhoLike > (peopleNum / 2) {
+		more = true
+	}
+	resp := experiment{
+		people: people,
+		more:   more,
+	}
+	return resp
 }
 
 func main() {
-    rightGuess := 0
-    for i := 0 ; i < 10000; i++{
-        exp := NewExperiment(10000)
-        guess := exp.people[0]
-        if exp.more == guess{
-            rightGuess++
-        }
-    }
-    fmt.Print(rightGuess)
+	numOfPeople := 10000
+	numOfExperiments := 10000
+	rightGuessByAlwaysTrue := 0
+	rightGuessByFirstPerson := 0
+	for i := 0; i < numOfExperiments; i++ {
+		exp := NewExperiment(numOfPeople)
+		guessByAlwaysTrue := true
+		guessByFirstPerson := exp.people[0]
+		if exp.more == guessByAlwaysTrue {
+			rightGuessByAlwaysTrue++
+		}
+		if exp.more == guessByFirstPerson {
+			rightGuessByFirstPerson++
+		}
+	}
+	fmt.Println("Guessting by believing it is always true",
+		rightGuessByAlwaysTrue,
+		" probability : ",
+		float64(rightGuessByAlwaysTrue)/float64(numOfPeople),
+	)
+	fmt.Println("Guessting by the first person's answer",
+		rightGuessByFirstPerson,
+		" probability : ",
+		float64(rightGuessByFirstPerson)/float64(numOfPeople),
+	)
 }
